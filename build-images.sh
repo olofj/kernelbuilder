5fa2db516d62372ff8bbb152414b1d2336799bbf
Olof Johansson <olof@lixom.net>
2018-10-31 11:12:37 -0700

    build-images: fix messed up version string
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- build-images.sh
+++ build-images.sh
@@ -3,4 +3,4 @@
 # Lazy script to rebuild all images.
 
 docker build --rm -t local/kernelbuilder:latest -t local/kernelbuilder:$(date +%Y%m%d_%H%M) .
-docker build --rm --build-arg GCCVER="\~7.3.0" -t local/kernelbuilder-7.3.0:latest -t local/kernelbuilder-7.3.0:$(date +%Y%m%d_%H%M) .
+docker build --rm --build-arg GCCVER="~7.3.0" -t local/kernelbuilder-7.3.0:latest -t local/kernelbuilder-7.3.0:$(date +%Y%m%d_%H%M) .
