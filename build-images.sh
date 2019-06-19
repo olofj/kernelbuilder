a2fa0d847267b655bad47fb1c6e302dc5c7090b6
Olof Johansson <olof@lixom.net>
2019-06-19 05:14:53 -0700

    build-images.sh: add 8.3.0 image too

--- build-images.sh
+++ build-images.sh
@@ -3,4 +3,5 @@
 # Lazy script to rebuild all images.
 
 docker build --rm -t local/kernelbuilder:latest -t local/kernelbuilder:$(date +%Y%m%d_%H%M) .
+docker build --rm --build-arg GCCVER="~8.3.0" -t local/kernelbuilder-8.3.0:latest -t local/kernelbuilder-8.3.0:$(date +%Y%m%d_%H%M) .
 docker build --rm --build-arg GCCVER="~7.3.0" -t local/kernelbuilder-7.3.0:latest -t local/kernelbuilder-7.3.0:$(date +%Y%m%d_%H%M) .
